a16c68530dd90116d64b7cdcea287b991532d8d8
Collin <cjsidler@gmail.com>
2023-11-19 11:31:37 -0800

    add types for JSON

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
+}
+
+type StoryArc struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
 func main() {
 	fmt.Println("Hello, world!")
 }
